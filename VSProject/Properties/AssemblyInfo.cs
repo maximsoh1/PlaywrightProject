6d871b263d73ccdd9ef11dbb15804ae1afbe4240
maxim.soh <maxim.soh@amdaris.com>
2026-02-19 16:51:31 +0200

    Enable Allure.NUnit for all tests via AssemblyInfo

---
+++ VSProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+using Allure.NUnit;
+using Allure.NUnit.Attributes;
+
+// Enable Allure for all tests in this assembly
+[assembly: AllureNUnit]
+
+// Set default display name for reports
+[assembly: AllureDisplayIgnored]
